6b82eb431c1d66a5ede62f90a76e1e3962460f74
Amir shiri <plus1319@outlook.com>
2020-01-17 14:52:31 +0330

    initial commit infra ioc project

---
+++ MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,16 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infra.Bust;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroRabbit.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Domain bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+
+        }
+    }
+}
